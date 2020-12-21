2de3a4f0b58722a65a29c7023171a8f1bc2b11c2
Hari_karthyk <Harikarthyk@users.noreply.github.com>
2020-12-21 08:46:23 +0530

    v-1

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const mysql = require("mysql");
 exports.db = mysql.createPool({
 	host: "db4free.net",
 	user: "hari_user_1",
-	//hari_user_1
 	password: "uTQ2vXwrr!9*V9H",
-	// uTQ2vXwrr!9*V9H
 	database: "hari_blog_1",
-	// hari_blog_1
 	port: 3306,
 });
